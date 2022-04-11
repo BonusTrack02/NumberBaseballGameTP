93160cc3083cbb78a3262e6fc5c4235592e15613
BonusTrackKR <wlsgud321@gmail.com>
2022-04-11 23:55:27 +0900

    Added BeginningActivity Added Adfit banner

--- app/src/main/java/com/bonustrack02/numberbaseballgametp/BeginningActivity.java
+++ app/src/main/java/com/bonustrack02/numberbaseballgametp/BeginningActivity.java
@@ -1,8 +1,13 @@
 package com.bonustrack02.numberbaseballgametp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 
 import com.bonustrack02.numberbaseballgametp.databinding.ActivityBeginningBinding;
 
@@ -15,5 +20,16 @@ public class BeginningActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityBeginningBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        handler.sendEmptyMessageDelayed(0, 2500);
     }
+
+    Handler handler = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            Intent intent = new Intent(BeginningActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    };
 }
