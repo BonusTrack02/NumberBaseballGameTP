e16b890e4b0fa1925962060159c5eb9a9321db96
BonusTrack02 <wlsgud321@gmail.com>
2023-03-03 01:02:11 +0900

    convert MainActivity.java to Kotlin add Material color theme update BeginningActivity has no status bar update adfit client id visibility using local.properties

--- app/src/main/java/com/bonustrack02/numberbaseballgametp/BeginningActivity.kt
+++ app/src/main/java/com/bonustrack02/numberbaseballgametp/BeginningActivity.kt
@@ -10,15 +10,15 @@ import androidx.appcompat.app.AppCompatActivity
 import com.bonustrack02.numberbaseballgametp.databinding.ActivityBeginningBinding
 
 class BeginningActivity : AppCompatActivity() {
-    val binding: ActivityBeginningBinding by lazy { ActivityBeginningBinding.inflate(layoutInflater) }
+    private val binding: ActivityBeginningBinding by lazy { ActivityBeginningBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
-        handler.sendEmptyMessageDelayed(0, 2500)
+        handler.sendEmptyMessageDelayed(0, 2000)
     }
 
-    val handler: Handler = object : Handler(Looper.getMainLooper()) {
+    private val handler: Handler = object : Handler(Looper.getMainLooper()) {
         override fun handleMessage(msg: Message) {
             val intent = Intent(this@BeginningActivity, MainActivity::class.java)
             startActivity(intent)
