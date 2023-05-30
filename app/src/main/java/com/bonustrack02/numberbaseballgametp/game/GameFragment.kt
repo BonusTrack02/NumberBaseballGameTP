80bf82b73ff9d7de8078d563546a9cc301423f23
BonusTrack02 <wlsgud321@gmail.com>
2023-05-31 00:45:56 +0900

    update GameFragment and ViewModel to apply MVVM architecture

--- app/src/main/java/com/bonustrack02/numberbaseballgametp/game/GameFragment.kt
+++ app/src/main/java/com/bonustrack02/numberbaseballgametp/game/GameFragment.kt
@@ -1,6 +1,8 @@
 package com.bonustrack02.numberbaseballgametp.game
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -23,11 +25,23 @@ class GameFragment : Fragment() {
         viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
         binding.gameViewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
+        binding.edit01.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.isNotEmpty() == true) binding.edit02.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        binding.edit02.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s?.isNotEmpty() == true) binding.edit03.requestFocus()
+            }
+            override fun afterTextChanged(s: Editable?) {}
+        })
+        viewModel.resultText.observe(viewLifecycleOwner) {
+            binding.resultText.text = it
+        }
         return binding.root
-    }
-
-    override fun onResume() {
-        super.onResume()
-        binding.lottieAnswer.visibility = View.VISIBLE
     }
 }
--- app/src/main/java/com/bonustrack02/numberbaseballgametp/game/GameViewModel.kt
+++ app/src/main/java/com/bonustrack02/numberbaseballgametp/game/GameViewModel.kt
@@ -1,13 +1,20 @@
 package com.bonustrack02.numberbaseballgametp.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import java.util.Random
 
 class GameViewModel : ViewModel() {
-    private var num1: Int = 0
-    private var num2: Int = 0
-    private var num3: Int = 0
+    private var num1 = 0
+    private var num2 = 0
+    private var num3 = 0
+    private var tryCount = 0
+    var stringBuilder = StringBuilder()
+    private val _resultText = MutableLiveData<String>()
+    val resultText: LiveData<String>
+        get() = _resultText
 
     init {
         Log.d("gameViewModel", "created")
@@ -28,5 +35,58 @@ class GameViewModel : ViewModel() {
         do {
             num3 = random.nextInt(9) + 1
         } while (num1 == num3 || num2 == num3)
+    }
+
+    fun onSubmit(edit01: String, edit02: String, edit03: String) {
+        Log.d("submit", edit01)
+        val tempAnswer1: Int
+        val tempAnswer2: Int
+        val tempAnswer3: Int
+        try {
+            tempAnswer1 = edit01.toInt()
+            tempAnswer2 = edit02.toInt()
+            tempAnswer3 = edit03.toInt()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // show toast
+            return
+        }
+        var strikeCount = 0
+        var ballCount = 0
+
+        when (num1) {
+            tempAnswer1 -> strikeCount++
+            tempAnswer2 -> ballCount++
+            tempAnswer3 -> ballCount++
+        }
+
+        when (num2) {
+            tempAnswer2 -> strikeCount++
+            tempAnswer1 -> ballCount++
+            tempAnswer3 -> ballCount++
+        }
+
+        when (num3) {
+            tempAnswer3 -> strikeCount++
+            tempAnswer1 -> ballCount++
+            tempAnswer2 -> ballCount++
+        }
+
+        tryCount++
+
+        // append result text
+        stringBuilder.append("시도 $tryCount : $tempAnswer1 $tempAnswer2 $tempAnswer3   ${strikeCount}Strike ${ballCount}Ball\n")
+        _resultText.value = stringBuilder.toString()
+        Log.d("result", stringBuilder.toString())
+
+        if (strikeCount == 3) {
+            // lottie image shown
+            // keypad hide
+        }
+
+        // editText value erase
+
+        // edit01 requestfocus
+        // scrollView fullscroll
     }
 }
